19ed80be4c81fe3b465036b7327b5436327c3eb4
Chris <foxcode9@gmail.com>
2020-08-26 21:20:42 +0200

    updates

--- Joesph_Falzon_Website/script.js
+++ Joesph_Falzon_Website/script.js
@@ -1,6 +1,14 @@
+// import smoothscroll from 'smoothscroll-polyfill'
+
+// smoothscroll.polyfill();
+// window.__forceSmoothScrollPolyfill__ = true;
+
+// _____________________________________________________________________
+
+
 window.document.addEventListener('scroll', ()=>{
     const {scrollTop} = document.documentElement;
-    console.log(scrollTop);
+    // console.log(scrollTop);
 
     if (scrollTop > 50) {
         // "navigation--background"
@@ -25,8 +33,6 @@ const slider = document.querySelector('.card2__scroll');
 let isDown = false;
 let startX;
 let scrollLeft;
-
-
 
 
 slider.addEventListener('mousedown', (e) => {
@@ -62,7 +68,49 @@ slider.addEventListener('mousemove', (e) => {
 
     const walk = x - startX;
 
-    console.log({x, startX, walk, isDown});
+    // console.log({x, startX, walk, isDown});
 
     slider.scrollLeft = (scrollLeft - walk) * 1;
+});
+
+
+// _____________________________________________________________________
+
+const btnNext = document.querySelector('.arrow__next-btn'); 
+
+btnNext.addEventListener('click', (e)=>{
+
+    
+    
+    console.log(slider.scrollLeft + slider.clientWidth)
+
+    
+    slider.scroll({
+        top: 0,
+        left: slider.scrollLeft + slider.clientWidth + 50 ,
+        behavior: 'smooth'
+      });     
+      
+    //   setTimeout(()=>{
+
+    //     window.setInterval(()=>{
+            
+    //     }, 1000/60)
+    //   }, 1000)
+    
+}); 
+
+const btnPrev = document.querySelector('.arrow__prev-btn'); 
+
+btnPrev.addEventListener('click', (e)=>{
+    
+    const rowWidth = document.querySelector('.arrow')
+
+    console.log(slider.clientWidth - slider.scrollLeft)
+    slider.scrollTo({
+        top: 0,
+        left: (slider.clientWidth - slider.scrollLeft + 50 ) * -1,
+        behavior: 'smooth'
+      });     
+    
 });
