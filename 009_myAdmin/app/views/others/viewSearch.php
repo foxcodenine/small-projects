acec8281755a6fc08fc387bb400a5a8d9ed2a932
Christopher Farrugia <foxcode9@gmail.com>
2022-03-18 13:33:30 +0100

    working on search page

--- 009_myAdmin/app/views/others/viewSearch.php
+++ 009_myAdmin/app/views/others/viewSearch.php
@@ -1,6 +1,8 @@
 <section class="search">
 
-    <form action="" class="search__query-form">
+    <!-- /////////////////////////////////////////////////////////// -->
+
+    <form action="" class="search__query-form search__hidden">
 
         <div class="search__fields">
             <input type="text" class="search__field" placeholder="firstname">
@@ -8,79 +10,156 @@
             <input type="text" class="search__field" placeholder="id">
             <input type="text" class="search__field" placeholder="company">
             <input type="text" class="search__field" placeholder="email">
-            <input type="text" class="search__field" placeholder="phone">
-            <input type="text" class="search__field" placeholder="mobile">
-            <input type="text" class="search__field" placeholder="street address">
+            <input type="text" class="search__field" placeholder="phone / mobile">
+            <input type="text" class="search__field search__field--span2" placeholder="street address">
             <input type="text" class="search__field" placeholder="locality">
             <input type="text" class="search__field" placeholder="country">
+
+            <button class="btn btn--dark search__btn--1" type="button">search</button>
         </div>
 
-        <button class="btn btn--red search__btn--1" type="button">remove client</button>
     </form>
+
+    <!-- /////////////////////////////////////////////////////////// -->
+
+    <form action="" class="search__query-form ">
+
+        <div class="search__fields">
+            
+            <input type="text" class="search__field search__field--span2" placeholder="Project Name">
+
+            <div class=" search__field search__field--span2 search__field--select" >
+                <select name="" id="" class="search__select" id="client" required>
+                    <option value="" selected hidden">Client...</option>
+                    <option value="">Dorothy Cassar</option>
+                    <option value="">Joelle Ellul</option>
+                    <option value="">Danine Leonardi</option>
+                </select>
+                <svg class="search__down-arrow">
+                    <use xlink:href="./app/static/svg/icomoon.svg#icon-arrow_drop_down"></use>
+                </svg>
+            </div>
+
+            <input type="text" class="search__field " placeholder="Street Address">
+            
+
+            <div class=" search__field search__field--select">
+                <select name="" id="" class="search__select" id="client" required>
+                    <option value="" selected hidden">Locality...</option>
+                    <option value="">Dorothy Cassar</option>
+                    <option value="">Joelle Ellul</option>
+                    <option value="">Danine Leonardi</option>
+                </select>
+                <svg class="search__down-arrow">
+                    <use xlink:href="./app/static/svg/icomoon.svg#icon-arrow_drop_down"></use>
+                </svg>
+            </div>
+
+
+            <input type="text" class="search__field" placeholder="Project No">
+            <input type="text" class="search__field" placeholder="PA No">
+
+            <div class=" search__field  search__field--select">
+                <select name="" id="" class="search__select" id="client" required>
+                    <option value="" selected hidden">Stage...</option>
+                    <option value="">Dorothy Cassar</option>
+                    <option value="">Joelle Ellul</option>
+                    <option value="">Danine Leonardi</option>
+                </select>
+                <svg class="search__down-arrow">
+                    <use xlink:href="./app/static/svg/icomoon.svg#icon-arrow_drop_down"></use>
+                </svg>
+            </div>
+            
+            <div class=" search__field  search__field--select" >
+                <select name="" id="" class="search__select" id="client"  required>
+                    <option value="" selected hidden">Category...</option>
+                    <option value="">Dorothy Cassar</option>
+                    <option value="">Joelle Ellul</option>
+                    <option value="">Danine Leonardi</option>
+                </select>
+                <svg class="search__down-arrow">
+                    <use xlink:href="./app/static/svg/icomoon.svg#icon-arrow_drop_down"></use>
+                </svg>
+            </div>
+
+
+
+            <button class="btn btn--dark search__btn--1" type="button">search</button>
+
+        </div>
+
+        
+
+    </form>
+
+    <!-- /////////////////////////////////////////////////////////// -->
 
     <form action=""  class="search__form" method="POST">
 
     <!-- 1 --------------------------------------------------------- -->
 
-    <div class="top-pannel">
-            <div class="top-pannel__date"><b>search</b></div>
+        <div class="top-pannel">
+                <div class="top-pannel__date"><b>search</b></div>
 
-        <div class="top-pannel__buttons">
-            <a href="<?= $_ENV['BASE_PATH'] ?>/clients-add" class="btn btn--light">add new client</a>
-            <button class="btn btn--red modalBtn" type="button">remove client</button>
+            <div class="top-pannel__buttons">
+                <a href="<?= $_ENV['BASE_PATH'] ?>/clients-add" class="btn btn--light">add new client</a>
+                <button class="btn btn--red modalBtn" type="button">remove client</button>
+            </div>
         </div>
-    </div>
 
 
     <!-- 2 --------------------------------------------------------- -->
 
 
-    <div class="search__tabs">
-        <a href="" class="search__tab search__tab--active">Clients</a>
-        <a href="" class="search__tab">Projects</a>
-    </div>
+        <div class="search__tabs">
+            <a href="" class="search__tab search__tab--active">Clients</a>
+            <a href="" class="search__tab">Projects</a>
+        </div>
 
     <!-- 3 --------------------------------------------------------- -->
 
-    <div class="search__errors">
-        error placeholder! .... error placeholder! ....
-    </div>
+        <div class="search__errors">
+            error placeholder! .... error placeholder! ....
+        </div>
 
-    
+        
 
     <!-- 4 --------------------------------------------------------- -->
 
-    <div class="search__placeholder"></div>
+        <div class="search__placeholder"></div>
 
     <!-- 5 --------------------------------------------------------- -->
 
 
-    <?php require './app/views/tables/viewClientsTable.php'?>
+        <?php require './app/views/tables/viewClientsTable.php'?>
 
 
     <!-- 6 --------------------------------------------------------- -->
 
-<section class="modal">
-    <div class="modal__content">
+        <section class="modal">
+            <div class="modal__content">
 
-        <svg class="modal__close"><use href="./app/static/svg/icomoon.svg#icon-x-mark-thin"></use></svg>
+                <svg class="modal__close"><use href="./app/static/svg/icomoon.svg#icon-x-mark-thin"></use></svg>
 
-        <div class="modal__title">Delete Confermation </div>
+                <div class="modal__title">Delete Confermation </div>
 
-        <p style="display: none;" class="modal-hidden-message-js">Are you sure you want to delete these Projects?</p>
+                <p style="display: none;" class="modal-hidden-message-js">Are you sure you want to delete these Projects?</p>
 
-        <div class="modal__question modal-question-js">Are you sure you want to delete these Projects?</div>
+                <div class="modal__question modal-question-js">Are you sure you want to delete these Projects?</div>
 
-        <div  class="modal__confirmation" method="POST">
-        
-            
-            <button class="btn btn--light modal__cancel" type="button">cancel</button>
-            <button class="btn btn--red myLoaderBtn" >Confirm</button>
-        </div>
+                <div  class="modal__confirmation" method="POST">
+                
+                    
+                    <button class="btn btn--light modal__cancel" type="button">cancel</button>
+                    <button class="btn btn--red myLoaderBtn" >Confirm</button>
+                </div>
 
-    </div>    
-</section>
+            </div>    
+        </section>
 
-</form>
+    </form>
+
+    <!-- /////////////////////////////////////////////////////////// -->
 
 </section>
